1de3b5948b44ac3802280b69a66d5b7c44c3a837
LittleWhole <wholelittle@gmail.com>
2022-05-08 11:48:31 -0400

    Logo, drawImageCentered

--- src/util/DrawUtilities.java
+++ src/util/DrawUtilities.java
@@ -1,6 +1,7 @@
 package util;
 
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
 import org.newdawn.slick.TrueTypeFont;
 import org.newdawn.slick.geom.Rectangle;
 
@@ -17,6 +18,15 @@ public final class DrawUtilities {
 
     public static Rectangle createRectangleCentered(float x, float y, float width, float height) {
         return new Rectangle(x - width / 2, y - height / 2, width, height);
+    }
+
+    public static void drawImageCentered(Graphics g, Image image, float x, float y) {
+        int width = image.getWidth();
+        int height = image.getHeight();
+        Rectangle r = createRectangleCentered(x, y, width, height);
+
+        g.drawImage(image, (r.getX() + r.getWidth() / 2) - (width / 2),
+                (r.getY() + r.getHeight() / 2) - (height / 2));
     }
 
     /*===================================*/
