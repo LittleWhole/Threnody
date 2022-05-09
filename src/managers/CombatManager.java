5f011668574a10d977c2920b8367aa05b19cbd59
Panda248 <p4nda248@gmail.com>
2022-05-09 12:19:00 -0400

    we f

---
+++ src/managers/CombatManager.java
@@ -0,0 +1,63 @@
+package managers;
+
+import entities.units.Enemy;
+import entities.units.Unit;
+import entities.units.player.Player;
+
+import java.util.ArrayList;
+
+public class CombatManager {
+
+    private ArrayList<Player> players;
+    private ArrayList<Enemy> enemies;
+
+    public CombatManager(ArrayList<Player> plrs, ArrayList<Enemy> enemies)  {
+        this.players = plrs;
+        this.enemies = enemies;
+    }
+
+    public char combat()    {
+        int round = 1;
+        boolean plrsAlive = true;
+        boolean enemiesAlive = true;
+        while(plrsAlive && enemiesAlive)    {
+            for(int i = 0; i < players.size(); i++) {
+                players.get(i).move();
+            }
+            for(int i = 0; i < enemies.size(); i++) {
+                enemies.get(i).battleMove();
+            }
+            if(!checkEnemiesAlive(enemies))   {
+                return 'w';
+            }
+            if(!checkPlayersAlive(players)) {
+                return 'l';
+            }
+            round++;
+        }
+        return 'd';
+    }
+
+    private boolean checkEnemiesAlive(ArrayList<Enemy> enemies)    {
+        for(Enemy u: enemies)  {
+            if(u.getHealth() <= 0)  {
+                return false;
+            }
+        }
+        return true;
+    }
+    private boolean checkPlayersAlive(ArrayList<Player> plrs)   {
+        for(Player u: plrs)  {
+            if(u.getHealth() <= 0)  {
+                return false;
+            }
+        }
+        return true;
+        /*if andrew = shorthair
+                then andrew= rat
+                        dsplay andrewnationality;
+        else{
+            return false;
+        }*/
+    }
+}
